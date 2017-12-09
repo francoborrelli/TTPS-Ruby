383550d3358b48a33ac1779cc53cfae19457f073
francoborrelli <francoborrelli96@outlook.com>
2017-12-09 17:40:57 -0300

    add authenticate filter to course controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
-  before_action :set_course, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :set_course,  only: [:show, :edit, :update, :destroy]
 
   # GET /courses
   def index
