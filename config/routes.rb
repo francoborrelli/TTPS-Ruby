ed6bc545973d7e0808b2d05899bcfad28991f5f0
francoborrelli <francoborrelli96@outlook.com>
2017-12-09 12:11:23 -0300

    Change routes for user actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #User routes
+  devise_for :users, skip: :all
+
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
+    post 'sign_in', to: 'devise/sessions#create', as: :user_session
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
+
+
 end
