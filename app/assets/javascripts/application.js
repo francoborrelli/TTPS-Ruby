501f173a0cbc98e33f0a60da2dfd493f1a1bafd0
francoborrelli <francoborrelli96@outlook.com>
2017-12-09 21:49:40 -0300

    Add delete course modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery3 
 //= require rails-ujs
-//= require turbolinks
 //= require_tree .
 //= require popper 
 //= require bootstrap-sprockets
+//= require turbolinks
--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -2,7 +2,20 @@
 // All this logic will automatically be available in application.js.
 
 $(document).ready(function(){
+
+    //Initialize page tooltips
     $('[data-toggle="tooltip"]').tooltip({
         placement: "right"
     }); 
+        
+    //Changes delete modal element
+    $('.btn-delete').click(function(e) {
+        e.preventDefault();
+        $btn = $('#delete-confirm').find('.btn-danger')
+        $href = $btn.attr('href');
+        $new = $href.replace($href.slice(9), $(this).data('id'));
+        $btn.attr('href', $new);
+        $('#delete-confirm').modal('show');
+    });
   });
+
