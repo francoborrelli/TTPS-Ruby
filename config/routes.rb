5cc6d997f24d235f05c4a30c075ede5c2a012306
francoborrelli <francoborrelli96@outlook.com>
2017-12-10 11:36:00 -0300

    Fix root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,25 @@
 Rails.application.routes.draw do
 
-  root to: "courses#index"
-
   resources :courses
   #User routes
   devise_for :users, skip: :all
+
+  #if user authenticated then root is courses page
+  authenticated :user do
+    root 'courses#index', as: :authenticated_root
+  end
 
   devise_scope :user do
     get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
     post 'sign_in', to: 'devise/sessions#create', as: :user_session
     delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+
+    #if user is not authenticated redirect to singIn
+    root :to => redirect("/sign_in")
   end
+
+
+
+
 
 end
