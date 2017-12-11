33264650cee9f1aca8e2368e947be7268d2a3094
francoborrelli <francoborrelli96@outlook.com>
2017-12-11 14:23:57 -0300

    Add dni field to student

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171210235641) do
+ActiveRecord::Schema.define(version: 20171211172207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,9 @@ ActiveRecord::Schema.define(version: 20171210235641) do
     t.bigint "course_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "dni"
     t.index ["course_id"], name: "index_students_on_course_id"
+    t.index ["dni"], name: "index_students_on_dni"
   end
 
   create_table "users", force: :cascade do |t|
