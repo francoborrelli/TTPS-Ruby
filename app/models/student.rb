1cd4184253417dd228a9fff8d9fa3e6cc0962032
francoborrelli <francoborrelli96@outlook.com>
2017-12-11 17:42:39 -0300

    Add dni validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,4 +5,5 @@ class Student < ApplicationRecord
   validates :surname, presence: true, length: { in: 0..50 }, format: { with: /\A[a-zA-Z]+\z/, message: :only_letters } 
   validates :s_number, presence: true, uniqueness: {scope: :course}, length: { in: 0..10 }, format: { with: /\A[1-9]\d{0,6}(|.[\/][1-9]\d{0,2})\z/, message: :invalid_s_number } 
   validates :email, presence: true, uniqueness: {scope: :course}, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i , message: :invalid_email}
+  validates :dni, presence: true, uniqueness: {scope: :course}, length: { in: 5..12 }, numericality: { only_integer: true }
 end
