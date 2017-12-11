1a1377d5100e315052009131fe6f8489a3b51ae2
francoborrelli <francoborrelli96@outlook.com>
2017-12-11 11:45:44 -0300

    Fix students display

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,31 +1,25 @@
 class StudentsController < ApplicationController
+
   before_action :set_student, only: [:show, :edit, :update, :destroy]
+  before_action :set_course, only: [:create, :index ]
+  before_action :authenticate_user!
 
-  # GET /students
   def index
-    @students = Student.all
-    @course = Course.find(params[:course_id])
+    @students = @course.students
   end
 
-  # GET /students/1
   def show
-    @student = Student.find(params[:student_id])
-    @course = @student.course
   end
 
-  # GET /students/new
   def new
     @student = Student.new
   end
 
-  # GET /students/1/edit
   def edit
   end
 
-  # POST /students
   def create
     @student = Student.new(student_params)
-    @course = Course.find(params[:course_id])
     @student.course = @course
     if @student.save
       redirect_to course_students_path(@course, @student), notice: 'Student was successfully created.'
@@ -34,7 +28,6 @@ class StudentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /students/1
   def update
     if @student.update(student_params)
       redirect_to @student, notice: 'Student was successfully updated.'
@@ -43,19 +36,20 @@ class StudentsController < ApplicationController
     end
   end
 
-  # DELETE /students/1
   def destroy
     @student.destroy
     redirect_to students_url, notice: 'Student was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_student
-      @student = Student.find(params[:id])
+      @student = Student.find(params[:student_id])
     end
 
-    # Only allow a trusted parameter "white list" through.
+    def set_course
+      @course = Course.find(params[:course_id])
+    end
+
     def student_params
       params.require(:student).permit(:surname, :name, :s_number, :email, :course_id)
     end
