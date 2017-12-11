3f95dbe07b883e79e40380ccf626e2217160ea97
francoborrelli <francoborrelli96@outlook.com>
2017-12-11 01:08:45 -0300

    Add student validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,8 @@
 class Student < ApplicationRecord
   belongs_to :course
+
+  validates :name, presence: true, length: { in: 0..50 }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :surname, presence: true, length: { in: 0..50 }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" } 
+  validates :s_number, presence: true, uniqueness: {scope: :course}, length: { in: 0..10 }, format: { with: /\A[1-9]\d{0,6}(|.[\/][1-9]\d{0,2})\z/, message: "invalid student number" } 
+  validates :email, presence: true, uniqueness: {scope: :course}
 end
