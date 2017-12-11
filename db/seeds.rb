051943cce13c4bd642978943c45475fe82a81803
francoborrelli <francoborrelli96@outlook.com>
2017-12-11 18:31:04 -0300

    Add Students to seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,15 @@
 
     User.create({:username => "admin", :password => "123456", :email => "admin@admin.com"})
 
-    for i in 0..5
-        Course.create(:year => (Date.today.year - 5 + i));
+    Course.create(:year => (Date.today.year ));
+    Course.create(:year => (Date.today.year - 1));
+
+    for i in 0..7
+        offset = rand(Course.count)
+        rand_record = Course.offset(offset).first
+        name = Faker::GameOfThrones.character.split(/\W+/)
+        email = name.first + "@" + name.last + ".com"
+        Student.create({:name => name.first, :surname => name.last, :course => rand_record, :dni => (39831178 + i).to_s , :email => email , :s_number => ("123123/3" + i.to_s)  })
     end
 
     
