726d6b47da07836d3803a916a92b964377156b66
francoborrelli <francoborrelli96@outlook.com>
2017-12-11 14:10:45 -0300

    Fix course select

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,13 +1,17 @@
 class Course < ApplicationRecord
     has_many :students, dependent: :delete_all
 
-    validates :year, presence: true, uniqueness: true, numericality: { only_integer: true, greater_than_or_equal_to: :minYear,  less_than_or_equal_to: :maxYear }
+    validates :year, presence: true, uniqueness: true, numericality: { only_integer: true, greater_than_or_equal_to: :min_year,  less_than_or_equal_to: :max_year }
 
-    def minYear
+    def min_year
         Date.today.year - 35
     end
 
-    def maxYear
+    def max_year
         Date.today.year + 1
+    end
+
+    def time_range
+        (self.min_year..self.max_year).to_a
     end
 end
