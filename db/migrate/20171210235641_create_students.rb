947ef1af82a9bda2c5c3d2598a276eaa7d95c108
francoborrelli <francoborrelli96@outlook.com>
2017-12-10 23:07:45 -0300

    Fix migration

--- db/migrate/20171210235641_create_students.rb
+++ db/migrate/20171210235641_create_students.rb
@@ -3,10 +3,9 @@ class CreateStudents < ActiveRecord::Migration[5.1]
     create_table :students do |t|
       t.string :surname                       , null: false
       t.string :name                          , null: false
-      t.integer :s_number                     , null: false
+      t.string :s_number                      , null: false
       t.string :email                         , null: false
       t.belongs_to :course, foreign_key: true , null: false
-
       t.timestamps
     end
   end
