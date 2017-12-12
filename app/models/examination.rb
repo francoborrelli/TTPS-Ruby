5bbac3e78dfeec0922d78c5fcb47c0544b179e80
francoborrelli <francoborrelli96@outlook.com>
2017-12-12 15:35:40 -0300

    Fix validations

--- app/models/examination.rb
+++ app/models/examination.rb
@@ -3,5 +3,5 @@ class Examination < ApplicationRecord
 
   validates :title, presence: true
   validates :date, presence: true
-  validates :min_score, presence: true, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than_or_equal_to => 0, :less_than_or_equal_to => 100}
+  validates :min_score, presence: true, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/, :message => :decimal_msg }, :numericality => {:greater_than_or_equal_to => 0, :less_than_or_equal_to => 100}
 end
