039d2bfec51b22563f52a41f046ff0d3c63ca35e
francoborrelli <francoborrelli96@outlook.com>
2017-12-12 00:02:31 -0300

    Add basic index template

--- app/models/examination.rb
+++ app/models/examination.rb
@@ -1,3 +1,8 @@
 class Examination < ApplicationRecord
   belongs_to :course
+
+  validates :title, presence: true
+  validates :date, presence: true
+  validates :s_number, presence: true, uniqueness: {scope: :course}
+
 end
