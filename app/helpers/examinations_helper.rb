202442c5f7745f8068324c666c0edd62d270fc66
francoborrelli <francoborrelli96@outlook.com>
2017-12-12 15:57:32 -0300

    Improve examinations_helper

--- app/helpers/examinations_helper.rb
+++ app/helpers/examinations_helper.rb
@@ -1,5 +1,5 @@
 module ExaminationsHelper
     def time_format(datetime)
-        datetime.strftime("%d-%m-%Y") unless datetime.blank?
+        datetime.strftime("%d/%m/%Y") unless datetime.blank?
     end
 end
