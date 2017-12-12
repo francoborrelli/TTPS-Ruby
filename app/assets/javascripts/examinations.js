3377938c22026e6186e7386d08a71cdba29fc518
francoborrelli <francoborrelli96@outlook.com>
2017-12-12 15:47:40 -0300

    Add datepicker restrictions

--- app/assets/javascripts/examinations.js
+++ app/assets/javascripts/examinations.js
@@ -1,13 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
+    console.log(fSDate);
 $(function() {
     $('input.datepicker').data({behaviour: "datepicker"}).datepicker({
         format: "dd/mm/yyyy",
         language: "es",
         autoclose: true,
         orientation: "auto right",
-        endDate : 'now',
+        startDate: fSDate, 
+        endDate: fEDate
     });
 });
 });
