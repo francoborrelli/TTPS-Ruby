4f822a0984dcc42e347da5647dd33e087e8093ef
francoborrelli <francoborrelli96@outlook.com>
2017-12-12 14:57:09 -0300

    Add datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,4 @@
 //= require popper 
 //= require bootstrap-sprockets
 //= require turbolinks
+//= require js/datepicker.min
--- app/assets/javascripts/examinations.js
+++ app/assets/javascripts/examinations.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+$(function() {
+    $('input.datepicker').data({behaviour: "datepicker"}).datepicker({
+        format: "dd/mm/yyyy",
+        language: "es",
+        autoclose: true,
+        orientation: "auto right",
+        endDate : 'now',
+    });
+});
+});
+
