fcebd12df1d83c20d8f0673954d1449bea184a07
francoborrelli <francoborrelli96@outlook.com>
2017-12-13 12:01:40 -0300

    Add score validations

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,6 +2,16 @@ class Score < ApplicationRecord
   belongs_to :examination
   belongs_to :student
 
+  validates :score,
+            presence: true,
+            format: { with: /\A\d+(?:\.\d{0,2})?\z/,
+                      message: :decimal_msg },
+            numericality: { greater_than_or_equal_to: 0,
+                            less_than_or_equal_to: 100 }
+
+  validates :examination, uniqueness: { scope: :student }
+  validates :student, uniqueness: { scope: :examination }
+
   scope :find_by_pair, ->(student, examination) { where(student: student, examination: examination) }
 
   def passed?
