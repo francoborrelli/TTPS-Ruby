a361ca45cc7355dc3f86b2f356168ef0ccac1f76
francoborrelli <francoborrelli96@outlook.com>
2017-12-16 20:46:42 -0300

    Change btn class

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,20 +2,21 @@ class Score < ApplicationRecord
   belongs_to :examination
   belongs_to :student
 
-  default_scope -> {joins(:student).order("surname, name")
-  } 
+  default_scope -> { joins(:student).order('surname, name') }
 
-  validates :score, :allow_blank => true,
-            format: { with: /\A\d+(?:\.\d{0,2})?\z/,
-                      message: :decimal_msg },
-            numericality: { greater_than_or_equal_to: 0,
-                            less_than_or_equal_to: 100 }, 
-            if: Proc.new {|c| c.score.present?}
+  scope :find_by_pair, ->(student, examination) {
+    where(student: student, examination: examination)
+  }
+
+  validates :score, allow_blank: true,
+                    format: { with: /\A\d+(?:\.\d{0,2})?\z/,
+                              message: :decimal_msg },
+                    numericality: { greater_than_or_equal_to: 0,
+                                    less_than_or_equal_to: 100 },
+                    if: proc { |c| c.score.present? }
 
   validates :examination, uniqueness: { scope: :student }
   validates :student, uniqueness: { scope: :examination }
-
-  scope :find_by_pair, ->(student, examination) { where(student: student, examination: examination) }
 
   def passed?
     if score.present?
@@ -24,5 +25,5 @@ class Score < ApplicationRecord
       false
     end
   end
-
+  
 end
