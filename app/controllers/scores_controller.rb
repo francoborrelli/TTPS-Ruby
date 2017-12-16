27b7ab6c97683e57b876242145e0e0bdb24049d0
francoborrelli <francoborrelli96@outlook.com>
2017-12-16 20:27:56 -0300

    Add translations to edit scores

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,5 +1,5 @@
 class ScoresController < ApplicationController
-  before_action :set_course, :set_examination, only: [:index, :update]
+  before_action :set_course, :set_examination, only: %i[index update]
 
   # GET /scores
   def index
@@ -9,31 +9,27 @@ class ScoresController < ApplicationController
   # PATCH/PUT /scores/1
   def update
     if @examination.update(score_params)
-      redirect_to course_examination_scores_path(@course, @examination), notice: 'Score was successfully updated.'
+      redirect_to course_examination_scores_path(@course, @examination), notice: (t :updated_scores)
     else
       render :index
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_score
-      @score = Score.find(params[:id])
-    end
 
-    def set_course
-      @course = Course.find(params[:course_id])
-    end
+  def set_course
+    @course = Course.find(params[:course_id])
+  end
 
-    def set_examination
-      @examination = Examination.find(params[:examination_id])
-    end
+  def set_examination
+    @examination = Examination.find(params[:examination_id])
+  end
 
-    def get_scores_ordered
-      @examination.scores.where.not('score' => nil).joins(:student).order("name, surname")
-    end
+  def get_scores_ordered
+    @examination.scores.where.not('score' => nil).joins(:student).order('name, surname')
+  end
 
-    def score_params
-      params.require(:examination).permit(scores_attributes: [:score, :id, :student_id, :_destroy]) 
-    end
+  def score_params
+    params.require(:examination).permit(scores_attributes: %i[score id student_id _destroy])
+  end
 end
