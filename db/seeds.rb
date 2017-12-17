9dc214fe8febb897c8424a359d41382a1657d8da
francoborrelli <francoborrelli96@outlook.com>
2017-12-16 21:04:32 -0300

    Fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,12 @@ Course.create year: (Time.zone.today.year - 1)
   course = Course.offset(offset).first
   name = Faker::HarryPotter.character.split(/\W+/)
   email = name.first + '@' + name.last + '.com'
-  Student.create(name: name.first, surname: name.last, course: course, dni: (39_831_178 + i).to_s, email: email, s_number: ('123123/3' + i.to_s))
+  Student.create(name: name.first, surname: name.last, course: course, dni: "#{39_831_178 + i}", email: email, s_number: ("137#{i}/#{i/10}"))
 end
 
 Course.all.each do |course|
   4.times do |i|
-    title = 'Test ' + i.to_s
+    title = "Test #{i}"
     date = Time.zone.today.change(year: course.year) + i.days
     score = rand(0..100)
     Examination.create(course: course, title: title, min_score: score, date: date)
