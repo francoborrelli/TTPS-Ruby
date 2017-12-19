46146967ae4ffad73d3b635fb6a11719e43c0539
francoborrelli <francoborrelli96@outlook.com>
2017-12-19 20:43:47 -0300

    Add error controller

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module App
 
     config.assets.paths << Rails.root.join('vendor', 'assets')
 
+    config.exceptions_app = self.routes
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
