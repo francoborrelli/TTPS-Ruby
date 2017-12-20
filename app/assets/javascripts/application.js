f58cc54b7bf3cac7963dc6a319cdc0473f220a7f
francoborrelli <francoborrelli96@outlook.com>
2017-12-20 20:35:48 -0300

    Delete cable files references

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery3
 //= require rails-ujs
 //= require popper
@@ -17,6 +5,9 @@
 //= require_tree .
 
 $(document).ready(() => {
+
+  //Initialize page tooltips
+
   $('[data-toggle="tooltip"]').tooltip()
   
   $(".btn-delete").click(function(e) {
@@ -30,12 +21,16 @@ $(document).ready(() => {
     $("#delete-confirm").modal("show")
   })
 
-  $error = $element => {
-    $div = $($element).closest(".field_with_errors")
-    $div.find("small").remove()
-    $div.removeClass("field_with_errors")
+  error = function(element) {
+    div = $(element).closest(".field_with_errors")
+    div.find("small").remove()
+    div.removeClass("field_with_errors")
   }
-  $("input").change(() => $error(this))
+  $("input").change(function(){
+    error(this)
+  })
 
-  $("select").change(() => $error(this))
+  $("select").change(function(){
+    error(this)
+  })
 })
