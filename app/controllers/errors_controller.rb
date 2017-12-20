db68a54cc88113c8c7bb7b5fa9c1e76b1e4f78fe
francoborrelli <francoborrelli96@outlook.com>
2017-12-20 20:57:05 -0300

    Refactor error controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,17 @@
 class ErrorsController < ApplicationController
+  
   def not_found
-    render(:error, locals: { code: 404, message: :not_found }, status: 404)
+    render_error(404, :not_found)
   end
 
   def internal_server_error
-    render(:error, locals: { code: 500, message: :server_error }, status: 500)
+    render_error(500, :server_error)
   end
+
+  private
+
+  def render_error(code, message)
+    render(:error, locals: { code: code, message: message }, status: code)
+  end
+  
 end
